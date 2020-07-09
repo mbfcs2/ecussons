26e1676a92da1189f696df2a0937c199cff08e58
Antony <antony.moreau@gmail.com>
2020-07-09 10:09:16 +0200

    Add credits to illustration

---
+++ src/Migrations/Version20191222153806.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191222153806 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE destinations CHANGE display_type_id display_type_id INT NOT NULL');
+        $this->addSql('ALTER TABLE user ADD login VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE destinations CHANGE display_type_id display_type_id INT DEFAULT 1 NOT NULL');
+        $this->addSql('ALTER TABLE user DROP login');
+    }
+}
